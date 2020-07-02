c677144f476d9ca29f9cfc7b48620c9b26488c5f
xinyxie <xinyxie@ap.xom.com>
2020-07-02 17:58:08 +0800

    Finish all practice.

--- src/practice1/index.js
+++ src/practice1/index.js
@@ -1,1 +1,5 @@
+import {ADDRESS, PORT, PATH, REGION} from './constant.js'
+import getURL from './utils.js'
 
+export const url = getURL(ADDRESS, PORT, PATH);
+export {REGION as region};
--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,20 @@
-export const getPoetry
+// export const getPoetry = () => fetch('https://v1.jinrishici.com/all.json', {
+//   method: 'GET'
+// }).then(response => response.json())
+//   .then(json => {
+//     let result = [];
+//     result.push(json.origin, json.author, json.content);
+//     return result;
+//   })
+//   .catch(err => console.log(err));
+
+export async function getPoetry() {
+  const response = await fetch('https://v1.jinrishici.com/all.json', {
+    method: 'GET'
+  });
+
+  const json = await response.json();
+  let result = [];
+  result.push(json.origin, json.author, json.content);
+  return result;
+}
